eaf46d53c147ad3ac79a272896494c0adcf6cbba
Prince <princekumarsingh016592@gmail.com>
2026-06-19 09:33:54 +0530

    array in js

---
+++ main.js
@@ -0,0 +1,19 @@
+console.log("Hello World!")
+
+//array in javascript
+var arr = [1,2,3,4,5,6,7,8,9,10];
+console.log(arr);
+console.log(arr[0]);
+console.log(arr[1]);
+console.log(arr[2]);
+console.log(arr[3]);
+console.log(arr[4]);
+console.log(arr[5]);
+console.log(arr[6]);
+console.log(arr[7]);
+console.log(arr[8]);
+console.log(arr[9]);
+
+for(var i=0;i<arr.length;i++){
+    console.log(arr[i]);
+}
